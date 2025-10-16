ab921b1b89cab604eff7d6b9f9e45998cafa4f2b
Samuel Adetsi <abdulrahmanadetsi@gmail.com>
2025-10-15 22:38:17 -0700

    add test cases to staging models to sure data integrity checks

--- agrosense_dbt/models/staging/stg_soil.sql
+++ agrosense_dbt/models/staging/stg_soil.sql
@@ -1,21 +1,21 @@
 with source as (
-    select * from {{ source('agrosense_source', 'soil') }}
+    select * from {{ source("agrosense_snowflake", "soil") }}
 ),
 
 final as (
     select
-        'id',
-        'timestamp' as soil_timestamp,
-        't_0_cm' as temperature_0cm,
-        't_10_cm' as temperature_10cm,
-        'moisture',
-        'ph_0_5cm',
-        'ph_5_15cm',
-        'ph_15_30cm',
-        'ph_30_60cm',
-        'ph_60_100cm',
-        'ph_100_200cm',
-        'created_at'
+        "id" as id,
+        "timestamp" as timestamp,
+        "t_0_cm" as t_0_cm,
+        "t_10_cm" as t_10_cm,
+        "moisture" as moisture,
+        "ph_0_5cm" as ph_0_5cm,
+        "ph_5_15cm" as ph_5_15cm,
+        "ph_15_30cm" as ph_15_30cm,
+        "ph_30_60cm" as ph_30_60cm,
+        "ph_60_100cm" as ph_60_100cm,
+        "ph_100_200cm" as ph_100_200cm,
+        "created_at" as created_at
     from source
 )
 
