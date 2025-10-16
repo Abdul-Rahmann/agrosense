3e47a281cacf3dd8e50d41c012029b7cc4bd21fb
Samuel Adetsi <abdulrahmanadetsi@gmail.com>
2025-10-15 22:38:17 -0700

    add test cases to staging models to sure data integrity checks

--- agrosense_dbt/models/staging/stg_weather.sql
+++ agrosense_dbt/models/staging/stg_weather.sql
@@ -1,18 +1,18 @@
 with source as (
-    select * from {{ source('agrosense_source', 'weather') }}
+    select * from {{ source("agrosense_snowflake", "weather") }}
 ),
 
 final as (
     select
-        'id' as weather_id,  -- No manual quotes needed!
-        'latitude',
-        'longitude',
-        'timestamp',
-        'temperature',
-        'humidity',
-        'solar_radiation',
-        'pressure',
-        'created_at'
+        "id" as id,
+        "latitude" as latitude,
+        "longitude" as longitude,
+        "timestamp" as timestamp,
+        "temperature" as temperature,
+        "humidity" as humidity,
+        "solar_radiation" as solar_radiation,
+        "pressure" as pressure,
+        "created_at" as created_at
     from source
 )
 
