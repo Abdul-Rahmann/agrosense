a2ab73f57c4854bc30c17085c54b0379fce859eb
Samuel Adetsi <abdulrahmanadetsi@gmail.com>
2025-10-19 08:50:52 -0700

    fix duplicated sensor issue

--- agrosense_dbt/models/intermediate/int_sensor_readings_unified.sql
+++ agrosense_dbt/models/intermediate/int_sensor_readings_unified.sql
@@ -21,9 +21,36 @@ soil as (
     from {{ ref('stg_soil') }}
 ),
 
+-- Deduplicate soil data - keep only ONE record per day
+soil_deduped as (
+    select
+        date_trunc('day', timestamp) as date_day,
+        soil_temp_surface,
+        soil_temp_10cm,
+        soil_moisture,
+        ph_surface,
+        row_number() over (
+            partition by date_trunc('day', timestamp)
+            order by timestamp desc
+        ) as rn
+    from soil
+),
+
+soil_final as (
+    select
+        date_day,
+        soil_temp_surface,
+        soil_temp_10cm,
+        soil_moisture,
+        ph_surface
+    from soil_deduped
+    where rn = 1
+),
+
+-- Keep weather at hourly granularity
 weather_rounded as (
     select
-        date_trunc('minute', timestamp) as timestamp_minute,
+        date_trunc('hour', timestamp) as timestamp_minute,
         latitude,
         longitude,
         air_temp,
@@ -31,16 +58,6 @@ weather_rounded as (
         solar_radiation,
         pressure
     from weather
-),
-
-soil_rounded as (
-    select
-        date_trunc('minute', timestamp) as timestamp_minute,
-        soil_temp_surface,
-        soil_temp_10cm,
-        soil_moisture,
-        ph_surface
-    from soil
 ),
 
 joined as (
@@ -57,8 +74,8 @@ joined as (
         s.soil_moisture,
         s.ph_surface
     from weather_rounded w
-    inner join soil_rounded s
-        on w.timestamp_minute = s.timestamp_minute
+    inner join soil_final s
+        on date_trunc('day', w.timestamp_minute) = s.date_day
 )
 
 select * from joined
